70274f41f2e09ab822e87508b27d85fb46967418
Anoh Gabriel Unimke <gunimke33@gmail.com>
2026-02-11 14:10:53 +0100

    initial commit: Techiefied Assigment

---
+++ intro.js
@@ -0,0 +1,4 @@
+const username = "Gabriel"
+const age = 28
+const Status = "my favourite programming language is Java Script"
+console.log("I'm", username, "a", age,"years old backend student and",Status)

8fe08c7968d7aa868c069251bd991637e966e942
Anoh Gabriel Unimke <gunimke33@gmail.com>
2026-02-23 23:03:00 +0100

    updated

--- appserver.js
+++ appserver.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
+
 const express = require('express')
+const path = require('path') // built in node module
 const app = express()
-const port = 3000
+const port = process.env.PORT;
 
 app.use(express.json())
+
+// --- NEW: Serve static files from the "public" folder ---
+app.use(express.static('public'));   
 
 app.get('/', (req, res) => {
   res.send('My Week2 API!')
@@ -15,7 +21,14 @@ app.get('/user/:id', (req, res) => {
 
 app.post('/user', (req, res) => {
 const { name, email } = req.body;
-res.send(`Hello ${name}!`)});
+if (!name || !email) {
+        return res.status(400).json({
+            error: "Missing Data",
+            message: "Both 'name' and 'email' are required to create a user."
+        });
+      }
+res.send(`Hello ${name}!`);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
